37258d3e52ca469743ed04dccecfd4f44520100e
shourovhasan~ <shourovhasan001@gmail@.com>
2025-01-05 02:07:57 +0600

    drop-dwn menu are complete in navbar for mobile responsive

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../assets/frontend_assets/assets";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
 
 function Navbar() {
+
+
+  const [visible,setVisible] = useState(false)
+
+
+
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <img src={assets.logo} className="w-36" alt="" />
@@ -31,9 +38,54 @@ function Navbar() {
        <img src={assets.search_icon} alt="" className="w-5 cursor-pointer" />
 <div className=" group relative">
     <img src={assets.profile_icon} className="w-5 cursor-pointer" alt="" />
+    <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+  <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+    <p className="cursor-pointer hover:text-black">My Profile</p>
+    <p className="cursor-pointer hover:text-black">Orders</p>
+    <p className="cursor-pointer hover:text-black">LogOut</p>
+  </div>
+ 
+</div>
+
+
+
+
+
+
+
+
+
+</div>
+
+<Link  to='/card'  className="relative" >
+
+<img src={assets.cart_icon} className="w-5 min-w-5" alt=""  />
+
+<p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]    " >10</p>
+
+</Link>
+
+<img onClick={()=>setVisible(true)}  src={assets.menu_icon} className="w-5 cursor-pointer sm:hidden"/>
+
+      </div>
+
+      {/* Sidebar for  small screens  */}
+
+      <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}
+>
+
+<div className="flex flex-col text-gray-600">
+  <div onClick={()=>setVisible(false)} className="flex items-center gap-4 p-3 cursor-pointer" >
+    <img src={assets.dropdown_icon} alt="" />
+    <p>Back</p>
+
+  </div>
 </div>
 
       </div>
+
+
+
     </div>
   );
 }

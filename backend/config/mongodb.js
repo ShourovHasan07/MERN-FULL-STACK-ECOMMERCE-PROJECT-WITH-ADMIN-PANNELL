53ac2dda781e3445453c75257ef32977910a22f6
shourovhasan~ <shourovhasan001@gmail@.com>
2025-01-28 00:20:43 +0600

    create register, login api and test whit thander clint api in backend part

---
+++ backend/config/mongodb.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () =>{
+
+mongoose.connection.on('connected',()=>{
+    console.log(" MR. Shourov your database is Connected ")
+})
+
+
+await mongoose.connect(`${process.env.MONGODB_URI}/mern_forever_ecommerce`)
+
+
+
+
+}
+
+export default connectDB
